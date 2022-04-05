1c8338f945d5b1e9e0f2e9fbef68d265186e0cc9
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-04-05 17:27:42 +0100

    ui added

--- FinalProject/main.java
+++ FinalProject/main.java
@@ -15,12 +15,35 @@ public class main {
 				if (inputFunction.equals("exit"))// Exit the Program
 				{
 					exit = true;
-					System.out.println("Program is now exiting.");
-					System.exit(0);
+					//System.out.println("Exiting program");
+					//System.exit(0);
+					break;
+				}
+				
+				if (inputFunction.equals("1"))
+				{
+					System.out.println("Enter Stop ID for starting stop here: ");
+					String startStop = inputScanner.nextLine();
+					System.out.println("Enter Stop ID for finishing stop here: ");
+					String finalStop = inputScanner.nextLine();
+					System.out.printf("start: " + startStop + " finish: " + finalStop);
+				}
+				if (inputFunction.equals("2")) 
+				{
+					System.out.print("Enter the stop name you are looking for here: ");
+					String stopName = inputScanner.nextLine();
+					System.out.printf("name:" + stopName);
+				}
+				if (inputFunction.equals("3")) 
+				{
+					System.out.print("Enter the arrival time here in the format hh:mm:ss: ");
+					String arrivalTime = inputScanner.nextLine();
+					System.out.printf("time:" + arrivalTime);
 				}
 			}
 		}
-		//System.out.println("hello");
+		inputScanner.close();
+		System.out.println("Program has now exited");
 	}
 }
 
